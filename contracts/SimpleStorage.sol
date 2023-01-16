b875aba8af3940a4100d4b57802c343855e8648c
PhDr-B <nested.for.loops@gmail.com>
2023-01-16 12:46:03 -0500

    added variables to contract

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -2,5 +2,11 @@
 pragma solidity ^0.8.7; //Always line after license, carrot means version given or greater
 
 contract SimpleStorage {
-    
+    //types: boolean, uint, int, address, bytes
+    uint256 favoriteNumber; //Gets initialized to zero
+
+    function store(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+    }
+
 }
