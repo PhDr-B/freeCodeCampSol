fa99ae36ce5ad816690905892cf928528461f4ab
PhDr-B <nested.for.loops@gmail.com>
2023-01-16 16:50:17 -0500

    recoded up to memory lesson

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -5,8 +5,23 @@ contract SimpleStorage {
     //types: boolean, uint, int, address, bytes
     uint256 favoriteNumber; //Gets initialized to zero
 
+    struct People {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    People[] public people;
+
     function store(uint256 _favoriteNumber) public {
         favoriteNumber = _favoriteNumber;
+    }
+
+    function retrieve() public view returns (uint256) {
+        return favoriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        people.push(People(_favoriteNumber, _name));
     }
 
 }
