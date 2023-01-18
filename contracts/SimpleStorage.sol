4a5af59ed17334218e5a862364c83c3404e1e411
PhDr-B <nested.for.loops@gmail.com>
2023-01-17 23:05:35 -0500

    expanded addPerson Function

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -5,6 +5,8 @@ contract SimpleStorage {
     //types: boolean, uint, int, address, bytes
     uint256 favoriteNumber; //Gets initialized to zero
 
+    mapping(string => uint256) public nameToFavoriteNumber;
+    
     struct People {
         uint256 favoriteNumber;
         string name;
@@ -20,8 +22,11 @@ contract SimpleStorage {
         return favoriteNumber;
     }
 
-    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+    //calldata, memory, storage are 3 of 6 places the EVM can store data. Storage is lasting type
+    //need to be explicit about the type of arrays
+    function addPerson(string memory _name, uint256 _favoriteNumber) public { //temporary data type: memory type can be reassigned, but calldata cannot
         people.push(People(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
     }
 
 }
