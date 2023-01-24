55435e190b5cdf2b30917aa34823269df259ba16
PhDr-B <nested.for.loops@gmail.com>
2023-01-23 22:10:58 -0500

    updated FundMe and created library file

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -3,18 +3,30 @@
 //Set a minimum funding value
 
 //SPDX-License-Identifier: MIT
-pragma solidity ^0.8.7;
+pragma solidity ^0.8.8;
+
+import "./PriceConverter.sol";
 
 contract FundMe {
 
-    uint256 public minimumUsd = 50;
+    using PriceCoverter for uint256;
+
+    uint256 public minimumUsd = 50 * 1e18;
     
+    address[] public funders;
+    mapping(address => uint256) public addressToAmountFunded;
+
     function fund() public payable {
         //Want to be able to set a minimum fund amount
         //1. How do we send ETH to this contract?
         //Contracts can hold funds like wallets do
-        require(msg.value >= minimumUsd, "Didn't send enough"); //1e18 == 1 eth in wei, also need to get ETH to USD for this (oracle)
+        
+        require(msg.value.getConversionRate() >= minimumUsd, "Didn't send enough"); //1e18 == 1 eth in wei, also need to get ETH to USD for this (oracle)
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] = msg.value;
     }
+
+
 
     //function withdraw(){}
 }
